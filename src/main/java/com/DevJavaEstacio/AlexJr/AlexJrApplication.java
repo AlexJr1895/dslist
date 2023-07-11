81b5fdef6aae710f371aa4f50e166c33b281bbf9
Alex Jr <alexjunior1895@icloud.com>
2023-07-10 21:43:02 -0300

    Project created

---
+++ src/main/java/com/DevJavaEstacio/AlexJr/AlexJrApplication.java
@@ -0,0 +1,13 @@
+package com.DevJavaEstacio.AlexJr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexJrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DevJavaEstacio/AlexJr/AlexJrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevJavaEstacio.AlexJr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
