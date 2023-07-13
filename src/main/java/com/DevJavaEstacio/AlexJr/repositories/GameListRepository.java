4498d6748aa45f434d04c2d21b3faf88e6c494ef
Alex Jr <alexjunior1895@icloud.com>
2023-07-12 23:01:09 -0300

    GET lists

---
+++ src/main/java/com/DevJavaEstacio/AlexJr/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.DevJavaEstacio.AlexJr.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.DevJavaEstacio.AlexJr.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+}
